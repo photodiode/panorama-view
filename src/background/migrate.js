bd643ea677ce37dbb1b6499019a668a0311d1eef
photodiode <16804570+photodiode@users.noreply.github.com>
2021-10-18 16:38:43 +0900

    Fix migrate function

--- src/background/migrate.js
+++ src/background/migrate.js
@@ -3,28 +3,27 @@
 
 // migrate to transformable groups
 export async function migrate() {
+	
+	// remove old panorama view tabs
+	let extensionTabs = await browser.tabs.query({url: browser.runtime.getURL('view.html')});
+	for (let tab of extensionTabs) {
+		browser.tabs.remove(tab.id);
+	}
+	// ----
 
 	const windows = await browser.windows.getAll({});
 
 	for (const window of windows) {
 		let groups = await browser.sessions.getWindowValue(window.id, 'groups');
 
-		// no need to migrate
-		if (groups == undefined) return;
-		if (groups[0].title != undefined) return;
-
-		// remove old panorama view tabs
-		let extensionTabs = await browser.tabs.query({url: browser.runtime.getURL('view.html')});
-		for (let tab of extensionTabs) {
-			browser.tabs.remove(tab.id);
+		if (groups) {
+			for (let group of groups) {
+				if (group.title == undefined) {
+					group.title        = group.name;
+					group.lastAccessed = group.lastMoved;
+				}
+			}
+			await browser.sessions.setWindowValue(window.id, 'groups', groups);
 		}
-		// ----
-
-		// update tab groups object
-		for (let i in groups) {
-			groups[i].title        = groups[i].name;
-			groups[i].lastAccessed = groups[i].lastMoved;
-		}
-		await browser.sessions.setWindowValue(window.id, 'groups', groups);
 	}
 }
