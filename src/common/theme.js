50e871e5341e5dbd779ba1e742fb860b7c7df523
photodiode <16804570+photodiode@users.noreply.github.com>
2023-03-21 13:37:43 +0900

    Be a bit more selective for when to attempt custom theme Temporary response to #140

--- src/common/theme.js
+++ src/common/theme.js
@@ -7,11 +7,30 @@ import {Color} from '/common/colors.js'
 const useDark = window.matchMedia("(prefers-color-scheme: dark)");
 
 
+function isNull(array) {
+	for (let i = 0; i < array.length; i++) {
+		if (array[i] == null || array[i] == null) {
+			console.log(i);
+			return true;
+		}
+	}
+	return false;
+}
+
+
 export async function set(themeType, theme) {
 
 	if (!themeType) {
 		if (!theme) theme = await browser.theme.getCurrent();
-		if (theme && theme.colors) {
+console.log(theme.colors);
+		if (theme && theme.colors && !isNull([
+			theme.colors.frame,
+			theme.colors.toolbar_field_text,
+			theme.colors.toolbar,
+			theme.colors.toolbar_text,
+			theme.colors.toolbar_field,
+			theme.colors.tab_line
+		])) {
 			setAll(theme);
 			themeType = 'custom';
 		}
@@ -52,25 +71,23 @@ async function setAll(theme) {
 
 	if (theme && theme.colors) {
 
+		let background = new Color(theme.colors.frame);
+
 		let shadow = new Color(theme.colors.toolbar_field_text);
 		    shadow.a = 0.15;
 
-		let mixed_toolbar = new Color(theme.colors.frame).mix(new Color(theme.colors.toolbar));
+		let group = background.mix(new Color(theme.colors.toolbar));
+
+		let group_text = new Color(theme.colors.toolbar_text);
 
 		let group_border = shadow;
-		if (new Color(theme.colors.frame).toGrayscale().r < 0.4) {
-			group_border = mixed_toolbar.mix(new Color(1, 1, 1, 0.1));
+		if (background.toGrayscale().r < 0.4) {
+			group_border = group.mix(new Color(1, 1, 1, 0.1));
 		}
 
 		let tab = new Color(theme.colors.toolbar_field);
 
-		let tab_active = new Color(theme.colors.tab_line);
-
-		if ((tab_active.distance(tab) < 0.3 &&
-		     tab_active.distance(mixed_toolbar) < 0.3) ||
-		     tab_active.a < 0.1) {
-			tab_active = new Color(priority([theme.colors.toolbar_text, theme.colors.toolbar_field_text]));
-		}
+		let tab_text = new Color(theme.colors.toolbar_field_text);
 
 		let tab_overlay = new Color(theme.colors.toolbar_field);
 		    tab_overlay.a = 0.8;
@@ -78,16 +95,23 @@ async function setAll(theme) {
 		let tab_hover = new Color(theme.colors.toolbar_field_text);
 		    tab_hover.a = 0.3;
 
+		let tab_active = new Color(theme.colors.tab_line);
+		if ((tab_active.distance(tab) < 0.3 &&
+		     tab_active.distance(group) < 0.3) ||
+		     tab_active.a < 0.1) {
+			tab_active = new Color(priority([theme.colors.toolbar_text, theme.colors.toolbar_field_text]));
+		}
+
 		const style = `.custom {
-	--color-background: ${theme.colors.frame};
+	--color-background: ${background.toCSS()};
 	--color-shadow: ${shadow.toCSS()};
 
-	--color-group: ${mixed_toolbar.toCSS()};
-	--color-group-text: ${theme.colors.toolbar_text};
+	--color-group: ${group.toCSS()};
+	--color-group-text: ${group_text.toCSS()};
 	--color-group-border: ${group_border.toCSS()};
 
 	--color-tab: ${tab.toCSS()};
-	--color-tab-text: ${theme.colors.toolbar_field_text};
+	--color-tab-text: ${tab_text.toCSS()};
 	--color-tab-overlay: ${tab_overlay.toCSS()};
 	--color-tab-hover: ${tab_hover.toCSS()};
 	--color-tab-active: ${tab_active.toCSS()};
